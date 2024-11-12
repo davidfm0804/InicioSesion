24d5c0104733f33d927f27dce628fc5400add79c
David <davidfarinamorena.guadalupe@alumnado.fundacionloyola.net>
2024-11-12 09:43:33 +0100

    Inicio de Sesion Actualizado

--- login.php
+++ login.php
@@ -1,17 +1,12 @@
 <?php
 // Configuración de la base de datos
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "InicioSesion";
+$servidor = "localhost";
+$usuario = "root";
+$contrasenia = "";
+$basededatos = "InicioSesion";
 
 // Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
+$sqli = new mysqli($servidor, $usuario, $contrasenia, $basededatos);
 
 // Obtener los datos ingresados en el formulario
 $correo = $_POST['correo'];
@@ -19,17 +14,13 @@ $contrasena = $_POST['contrasena'];
 
 // Consultar en la base de datos
 $sql = "SELECT * FROM usuarios WHERE correo = '$correo' AND contrasena = '$contrasena'";
-$result = $conn->query($sql);
+$resultado = $sqli->query($sql);
 
-if ($result->num_rows > 0) {
+if ($resultado->num_rows > 0) {
     echo "Inicio de sesión exitoso. Bienvenido, " . $correo;
-    // Aquí puedes iniciar una sesión y redirigir al usuario a otra página
-    // session_start();
-    // $_SESSION['correo'] = $correo;
-    // header("Location: pagina_protegida.php");
 } else {
     echo "Correo o contraseña incorrectos.";
 }
 
-$conn->close();
+$sqli->close();
 ?>
